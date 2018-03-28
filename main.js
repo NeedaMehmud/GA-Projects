5106418a4d303b5dc0f2cf7c63d6e3875e2baec7
Needa Mehmud <needa.mehmud@gmail.com>
2018-03-27 22:03:37 -0400

    Unit 7
    lesson 1-4, small project, unit review guide

--- main.js
+++ main.js
@@ -1,26 +1,6 @@
-function addItem() {
-  var input = document.getElementsByTagName('input')[0];
-  var newItemText = input.value;
-  input.value = '';
-
-  var newItem = document.createElement('li');
-  newItem.innerHTML = newItemText;
-  document.getElementById('todoList').appendChild(newItem);
-  newItem.addEventListener('click', completeItem);
-}
-
-function completeItem() {
-  removeItemFromTodos(this);
-  addItemToCompleted(this);
-}
-
-function removeItemFromTodos(itemElement) {
-  document.getElementById('todoList').removeChild(itemElement);
-}
-
-function addItemToCompleted(itemElement) {
-  document.getElementById('completedList').appendChild(itemElement);
+$('.faq a').on('click', function(e) {
+    e.preventDefault();
+    $(this).parents('.info-item').toggleClass('active');
+      $(this).parents('.info-item').siblings().removeClass('active');
+});
   
-}
-
-document.getElementsByTagName('button')[0].addEventListener('click', addItem);
