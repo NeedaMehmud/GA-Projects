48d492af86be8fb233e02af583704109a5458153
Needa Mehmud <needa.mehmud@gmail.com>
2018-03-27 22:00:02 -0400

    Unit 5
    Lesson 1-5 small projects, unit review guide

--- main.js
+++ main.js
@@ -1,49 +1,26 @@
-var grungeAlbums = {
-  "albums":[
-    {
-        "name": "Bleach",
-        "artist": "Nirvana",
-        "unitsSold": 1700000
-    },
-    {
-        "name": "Nevermind",
-        "artist": "Nirvana",
-        "unitsSold": 30000000
-    },
-    {
-        "name": "In Utero",
-        "artist": "Nirvana",
-        "unitsSold": 15000000
-    },
-    {
-        "name": "Ten",
-        "artist": "Pearl Jam",
-        "unitsSold": 10000000
-    },
-    {
-        "name": "Vs",
-        "artist": "Pearl Jam",
-        "unitsSold": 6100000
-    },
-    {
-        "name": "Vitalogy",
-        "artist": "Pearl Jam",
-        "unitsSold": 4770000
-    }
-  ]
-};
+function addItem() {
+  var input = document.getElementsByTagName('input')[0];
+  var newItemText = input.value;
+  input.value = '';
 
-
-var grungeAlbumsJSON = JSON.stringify(grungeAlbums);
-var grungeAlbumsObject = JSON.parse(grungeAlbumsJSON);
-
-
-console.log(grungeAlbumsJSON);
-console.log(grungeAlbumsObject);
-
-for(var i=0; i< grungeAlbumsObject["albums"].length; i++){
-  var currentAlbum = grungeAlbumsObject["albums"][i];
-  console.log('Album: ' + currentAlbum.name);
-  console.log('Artist: ' + currentAlbum.artist);
-  console.log('Units sold: ' + currentAlbum.unitsSold);
+  var newItem = document.createElement('li');
+  newItem.innerHTML = newItemText;
+  document.getElementById('todoList').appendChild(newItem);
+  newItem.addEventListener('click', completeItem);
 }
+
+function completeItem() {
+  removeItemFromTodos(this);
+  addItemToCompleted(this);
+}
+
+function removeItemFromTodos(itemElement) {
+  document.getElementById('todoList').removeChild(itemElement);
+}
+
+function addItemToCompleted(itemElement) {
+  document.getElementById('completedList').appendChild(itemElement);
+  
+}
+
+document.getElementsByTagName('button')[0].addEventListener('click', addItem);
